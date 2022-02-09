871ed283a68ed9e7b2fd968dcfcdb7a67ecb39dd
Gabriel Lima <gpereira@blueedtech.com.br>
2022-02-09 20:37:12 -0300

    revisao aula 18

--- Aula 18/aula18_revisao.js
+++ Aula 18/aula18_revisao.js
@@ -2,12 +2,14 @@ const prompt = require('prompt-sync')();
 console.clear();
 console.log();
 
+/*
 let aluno = {
     nome: 'Daniel',
     turma: 'C11',
     idade: 20,
     cidade: 'Londrina',
     interesses: ['Cuidar do filho','Descansar'],
+    forca: 10,
 
     trocaCidade: function(){
         let estado = prompt('É no mesmo estado? ')
@@ -25,9 +27,45 @@ let aluno = {
     biografia: function(){
         console.log(`Nome: ${this.nome}
         Turma: ${this.turma}
-        Idade: ${this.idade}`)
+        Idade: ${this.idade}
+        Força: ${this.forca}`)
+    },
+
+    envelhecer: function(anos){
+        this.idade = this.idade + anos
+    },
+
+    aumentaForca: function(str) {
+        this.forca = this.forca + str
     }
 }
+
+
+// aluno.biografia();
+
+// let escolhaforca = +prompt('Aumentou quanto de força? ');
+// aluno.aumentaForca(escolhaforca);
+// aluno.envelhecer(30);
+
+// aluno.biografia();
+*/
+
+
+
+
+/*
+// Usando random para escolher um valor em uma array
+let lista = ['Encontrar novo planeta','Explorar meteoro','Investigar anomalia'];
+let navegacao = prompt('Deseja navegar? ')
+if (navegacao == 's'){
+    let rdm = Math.floor(Math.random() * 9);
+    console.log(rdm);
+    if (rdm <= 5){
+        console.log(lista[0])
+    }
+}
+*/
+
 
 
 
@@ -61,11 +99,32 @@ if (numero == 1) {
 }
 */
 
+
+/*
 // Usanhdo um for para percorrer uma lista
 let itens = ['Espada','Escudo','Maça','Machado']
 console.log('Escolha seu item: ')
 for (let i of itens){
     console.log(i)
 }
+*/
 
-console.log();
+
+/*
+// Exemplo de controle de horas
+let hora = 19
+let dia = 1
+console.log(hora);
+
+function dataHora(){
+    hora = hora + 4;
+    if (hora >= 24){
+        dia = dia + 1;
+        hora = hora - 24;
+    }
+    console.log(`São ${hora} h do dia ${dia}`);
+}
+
+dataHora()
+console.log(hora);
+*/
